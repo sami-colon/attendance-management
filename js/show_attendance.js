a75d7cbb560b43f33431012a19934395511752b7
sami-colon <abhishek.personal.email@gmail.com>
2019-10-08 17:28:09 +0530

    added filter feature in show attendance

--- js/show_attendance.js
+++ js/show_attendance.js
@@ -32,12 +32,99 @@ $(document).ready(function () {
             if (result['error'] === "true") {
                 alert("Records not present! Ask your teacher to update your attendance!");
             } else {
-                console.log(result);
                 for (var i = 0; i < result.length; i++) {
                     var html = "<tr><td>" + result[i].date + "</td><td>" + result[i].period + "</td><td>" + result[i].status + "</td><td>" + result[i].subject + "</td></tr>";
                     $('#tbody').append(html);
                 }
             }
+        }
+    });
+    $('#filterby').on('change', function (event) {
+        event.preventDefault();
+        if($('#filterby').val() == 'subject')
+        {
+            $.ajax({
+                url: "https://neweradevelopers.com/attendance/attendance_show.php",
+                type: "POST",
+                crossDomain: true,
+                data: {
+                    id: localStorage.getItem('id'),
+                    subject: 'subject'
+                },
+                dataType: "json",
+                success:function(result) {
+                    if (result['error'] === "true") {
+                        alert("Records not present! Ask your teacher to update your attendance!");
+                    } else {
+                        $('#tbody').empty();
+                        var subject_array = ["physics", "chemistry", "m1", "dbms", "oopm", "cn"];
+                        for(var i=0;i<subject_array.length;i++)
+                        {
+                            var html = "<tr><td colspan='4' class='bg-primary'>"+subject_array[i]+"</td></tr>";
+                            $('#tbody').append(html);
+                            for(var j=0;j<result[subject_array[i]].length;j++)
+                            {
+                                html = "<tr><td>" + result[subject_array[i]][j].date + "</td><td>" + result[subject_array[i]][j].period + "</td><td>" + result[subject_array[i]][j].status + "</td><td>" + result[subject_array[i]][j].subject + "</td></tr>";
+                                $('#tbody').append(html);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        else if($('#filterby').val() == 'status')
+        {
+            $.ajax({
+                url: "https://neweradevelopers.com/attendance/attendance_show.php",
+                type: "POST",
+                crossDomain: true,
+                data: {
+                    id: localStorage.getItem('id'),
+                    status: 'status'
+                },
+                dataType: "json",
+                success:function(result) {
+                    if (result['error'] === "true") {
+                        alert("Records not present! Ask your teacher to update your attendance!");
+                    } else {
+                        $('#tbody').empty();
+                        var status_array = ["present", "absent"];
+                        for(var i=0;i<status_array.length;i++)
+                        {
+                            var html = "<tr><td colspan='4' class='bg-primary'>"+status_array[i]+"</td></tr>";
+                            $('#tbody').append(html);
+                            for(var j=0;j<result[status_array[i]].length;j++)
+                            {
+                                html = "<tr><td>" + result[status_array[i]][j].date + "</td><td>" + result[status_array[i]][j].period + "</td><td>" + result[status_array[i]][j].status + "</td><td>" + result[status_array[i]][j].subject + "</td></tr>";
+                                $('#tbody').append(html);
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        else
+        {
+            $.ajax({
+                url: "https://neweradevelopers.com/attendance/attendance_show.php",
+                type: "POST",
+                crossDomain: true,
+                data: {
+                    id: localStorage.getItem('id')
+                },
+                dataType: "json",
+                success:function(result) {
+                    if (result['error'] === "true") {
+                        alert("Records not present! Ask your teacher to update your attendance!");
+                    } else {
+                        $('#tbody').empty();
+                        for (var i = 0; i < result.length; i++) {
+                            var html = "<tr><td>" + result[i].date + "</td><td>" + result[i].period + "</td><td>" + result[i].status + "</td><td>" + result[i].subject + "</td></tr>";
+                            $('#tbody').append(html);
+                        }
+                    }
+                }
+            });
         }
     });
 });
